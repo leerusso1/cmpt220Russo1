2a2f7add1a57b4661ba60e1e25ac874acbc6b437
Juan Arias <juan.arias@marist.edu>
2018-02-26 23:13:49 -0500

    Feedback

--- labs/Excercise8_7.java
+++ labs/Excercise8_7.java
@@ -27,6 +27,6 @@ public class Excercise8_7 {
 	public static double distance(
 		double x1, double y1, double z1, double x2, double y2, double z2) {
 		return Math.sqrt(Math.pow(x2 - x1, 2) +  
-			Math.pow(y2 - y1, 2) + Math.pow(y2 - y1, 2));
+			Math.pow(y2 - y1, 2) + Math.pow(z2 - z1, 2)); // JA
 	}
 }
